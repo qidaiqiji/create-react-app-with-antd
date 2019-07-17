59e7fae1492806ec126d26044333726f49687617
lixueyan <lixueyan@xiaomei360.com>
2019-07-17 11:07:24 +0800

    initial commit

---
+++ src/test/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+class Test extends Component {
+    render() {
+        return (
+            <div>
+                <Button type="primary">点击</Button>
+            </div>
+        )
+    }
+}
+export default Test;
